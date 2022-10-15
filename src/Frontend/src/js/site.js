850cd3ec10e1d761584a4a395228ebb81909c328
Nova <nova@suprnova.dev>
2022-10-15 19:13:16 -0400

    webpack and scss implementation (#19, #20) todo: webhook implementation is currently broken due to this. fix before pr

---
+++ src/Frontend/src/js/site.js
@@ -0,0 +1,13 @@
+// js vendors
+import '@popperjs/core';
+import 'bootstrap';
+import 'twemoji';
+
+// css vendors
+import 'bootstrap/dist/css/bootstrap.css';
+
+// css
+import '../css/site.scss';
+import twemoji from 'twemoji';
+
+twemoji.parse(document.body);
---
+++ src/Frontend/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+    entry: {
+        site: './src/js/site.js',
+        index: './src/js/index.js',
+        user_search: './src/js/user_search.js',
+        user_page: './src/js/user_page.js',
+        webhooks: './src/js/webhooks.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, '..', 'wwwroot', 'js')
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /.s?css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(eot|woff(2)?|ttf|otf|svg)$/i,
+                type: 'asset'
+            }
+        ]
+    },
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin()
+        ],
+        minimize: true
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "../css/[name].css"
+        })
+    ]
+};
